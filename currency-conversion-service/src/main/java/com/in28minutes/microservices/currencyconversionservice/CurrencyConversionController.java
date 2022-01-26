d3f18093c6da6ecd3552734baec52826c26237ae
Your Name <you@example.com>
2022-01-26 12:44:17 +0530

    connecting cs and ce

--- currency-conversion-service/src/main/java/com/in28minutes/microservices/currencyconversionservice/CurrencyConversionController.java
+++ currency-conversion-service/src/main/java/com/in28minutes/microservices/currencyconversionservice/CurrencyConversionController.java
@@ -1,10 +1,14 @@
 package com.in28minutes.microservices.currencyconversionservice;
 
 import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.Map;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class CurrencyConversionController {
@@ -14,12 +18,20 @@ public class CurrencyConversionController {
 				@PathVariable String from,
 				@PathVariable String to,
 				@PathVariable BigDecimal quantity) {
+		
+		Map<String, String> urlVariables=new HashMap<>();
+		urlVariables.put("from", from);
+		urlVariables.put("to", to);
+		ResponseEntity<CurrencyConversion> responseEntity = new RestTemplate().
+				getForEntity("http://localhost:8000/currency-exchange/from/{from}/to/{to}", 
+				CurrencyConversion.class,urlVariables);
+		CurrencyConversion currencyConversion = responseEntity.getBody();
 		return new CurrencyConversion(1000L,
 									  from,
 									  to,
 									  quantity,
-									  BigDecimal.valueOf(65),
-									  BigDecimal.valueOf(650),
+									  currencyConversion.getConversionMultiple(),
+									  quantity.multiply(currencyConversion.getConversionMultiple()),
 									  "8100");
 		
 	}
